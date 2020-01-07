9791673132f68ff69f9774dcd60d799875991efa
Jonh-Nash <vicious8424687@gmail.com>
2020-01-07 22:34:02 +0900

    [update]個人投稿一覧表示の作成
    個人投稿のルーティングとビューの作成をした。

--- diary/views.py
+++ diary/views.py
@@ -1,9 +1,11 @@
 import logging
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from django.urls import reverse_lazy
 from django.views import generic
 from .forms import InquiryForm
+from .models import Diary
 
 logger = logging.getLogger(__name__)
 
@@ -20,4 +22,14 @@ class InquiryView(generic.FormView):
         messages.success(self.request, 'メッセージを送信しました。')
         logger.info('inquiry sent by {}'.format(form.cleaned_data['name']))
         return super().form_valid(form)
+
+    class DiaryListView(LoginRequiredMixin, generic.ListView):
+        model = Diary
+        template_name = 'diary_list.html'
+
+        def get_queryset(self):
+            diaries = Diary.objects.filter(user=self.request.user).order_by('created_at')
+            return diaries
+
+
 
