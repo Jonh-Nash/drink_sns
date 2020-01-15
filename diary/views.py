398131faa55cd35b9bb8d8c1cbc0884b003380aa
Jonh-Nash <vicious8424687@gmail.com>
2020-01-15 20:33:31 +0900

    [update]投稿削除後の遷移ページの変更
    投稿削除後の遷移ページをdiary-alllistに変更した。

--- diary/views.py
+++ diary/views.py
@@ -9,8 +9,10 @@ from .models import Diary
 
 logger = logging.getLogger(__name__)
 
+
 class IndexView(generic.TemplateView):
     template_name = "index.html"
+
 
 class InquiryView(generic.FormView):
     template_name = "inquiry.html"
@@ -23,6 +25,7 @@ class InquiryView(generic.FormView):
         logger.info('inquiry sent by {}'.format(form.cleaned_data['name']))
         return super().form_valid(form)
 
+
 class DiaryListView(LoginRequiredMixin, generic.ListView):
     model = Diary
     template_name = 'diary_list.html'
@@ -32,9 +35,11 @@ class DiaryListView(LoginRequiredMixin, generic.ListView):
         diaries = Diary.objects.filter(user=self.request.user).order_by('created_at')
         return diaries
 
+
 class DiaryDetailView(LoginRequiredMixin, generic.DetailView):
     model = Diary
     template_name = 'diary_detail.html'
+
 
 class DiaryCreateView(LoginRequiredMixin, generic.CreateView):
     model = Diary
@@ -78,7 +83,7 @@ class DiaryUpdateView(LoginRequiredMixin, generic.UpdateView):
 class DiaryDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Diary
     template_name = 'diary_delete.html'
-    success_url = reverse_lazy('diary:diary_list')
+    success_url = reverse_lazy('diary:diary_alllist')
 
     def delete(self, request, *args, **kwargs):
         messages.success(self.request, '投稿を削除しました。')
