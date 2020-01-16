f1f4b8cc4899c4a69b02f93b43f61a8a41928bac
Jonh-Nash <vicious8424687@gmail.com>
2020-01-16 22:32:33 +0900

    [add]プロフィール情報にアイコンを追加
    プロフィール情報にアイコンを追加するためにユーザーモデルにアイコンフィールドを追加した。

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class CustomUser(AbstractUser):
+    icon = models.ImageField(
+        upload_to='img/',
+        verbose_name='アイコン',
+        blank=True,
+    )
     class Meta:
         verbose_name_plural = 'CustomUser'
